c697ffd897cac9818db1e5d817f1c497d39ba6cb
Roberto Alejandro Chávez Medina <robertomedina13018@gmail.com>
2019-03-30 22:26:31 -0600

    aiuda

--- Mod_vuelo.py
+++ Mod_vuelo.py
@@ -3,10 +3,16 @@ import cv2
 import time
 
 tello = Tello()
-tello.connect()
-tello.takeoff()
-time.sleep(5)
-tello.land()
-time.sleep(5)
-tello.end()
 
+def tello_control():
+    tello.takeoff()
+    time.sleep(5)
+    tello.land()
+    time.sleep(5)
+    tello.end()
+
+def main():
+    tello.connect()
+    tello_control()
+if __name__ == '__main__':
+    main()
